1e74b2d32ca7f9b476fbe47618cbaca85ff420a0
Razin Shaikh <razinshaikh@outlook.com>
2017-08-15 13:45:53 +0530

    Added ArticleThumbnails view

--- activity/models.py
+++ activity/models.py
@@ -4,6 +4,9 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     thumbnail = models.FileField()
     body = models.CharField(max_length=5000)
-
+    '''
+    organisation
+    author
+    '''
     def __str__(self):
         return self.title
